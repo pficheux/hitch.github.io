8074958b808f15c7a76bf4dde50643c8a03a88b8
Pierre FICHEUX <pierre.ficheux@smile.fr>
2022-07-17 11:15:15 +0200

    Initial import

---
+++ recommandation.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Recommandation OK !</title>
+<style type="text/css">
+<!--
+a:link{text-decoration:underline;text-transform:none;color:"000000"}
+a:hover{text-decoration:underline;text-transform:none;color:0000FF}
+-->
+</style>
+<meta name="generator" content="Namo WebEditor v5.0">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<p align="center"><b><font size="3" face="Verdana, Arial, Helvetica, sans-serif"><br>
+  <br>
+  Merci de votre recommandation 
+    ! <br>
+  <br>
+  <br>
+  Elle a été envoyée avec succès à cette adresse 
+  :<br>
+  <br>
+    &nbsp; </font></b></p>
+<br>
+<p align="center">  <a href="http://hitchcock.alienor.fr/"><font face="Verdana, Arial, Helvetica, sans-serif" size="2">RETOUR</font></a></p>
+</body>
+</html>
